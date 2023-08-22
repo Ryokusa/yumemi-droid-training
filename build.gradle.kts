61653796f697c2139f0ab2183e7680f7a82c7d0c
h_ideura <h_ideura@yumemi.co.jp>
2023-08-22 13:48:40 +0900

    upgrade: composeBOMアップグレードのためにkotlinをアップグレード

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra { "1.7.20" }
+    val kotlinVersion by extra { "1.8.21" }
     repositories {
         google()
         mavenCentral()
