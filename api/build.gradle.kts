2412e3b0e0c5615ab795620ef4ac3af23b7cada1
h_ideura <h_ideura@yumemi.co.jp>
2023-08-22 14:27:57 +0900

    upgrade: apiモジュールのcompile sdkアップグレード

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 
 android {
     namespace = "jp.co.yumemi.api"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
