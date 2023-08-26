a0fe4eb61534df8eb27f35185a782eaea3564bea
h_ideura <h_ideura@yumemi.co.jp>
2023-08-26 14:04:09 +0900

    add: 再利用しやすくコンポーネント分け

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,6 +20,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,32 +39,41 @@ fun WeatherApp(){
 
 @Composable
 fun CenterImage() {
-    BoxWithConstraints {
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         val screenWidth = with(LocalDensity.current) { constraints.maxWidth.toDp() }
         val width = screenWidth / 2
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.foo),
-                contentDescription = "MainImage",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .width(width)
-                    .clip(RectangleShape)
-            )
-            Row(modifier = Modifier.width(width)) {
-                Text(text = "left", textAlign = TextAlign.Center, modifier = Modifier.weight(1f))
-                Text(text = "right", textAlign = TextAlign.Center, modifier = Modifier.weight(1f))
-            }
+        MainImage(width = width)
+    }
+}
+@Composable
+fun MainImage(width: Dp){
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.foo),
+            contentDescription = "MainImage",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(width)
+                .clip(RectangleShape)
+        )
+        Row(modifier = Modifier.width(width)) {
+            Text(text = "left", textAlign = TextAlign.Center, modifier = Modifier.weight(1f))
+            Text(text = "right", textAlign = TextAlign.Center, modifier = Modifier.weight(1f))
         }
     }
 }
 
 @Composable
 @Preview
-fun PreviewCenterImage(){
+fun PreviewWeatherApp(){
     WeatherApp()
+}
+
+@Composable
+@Preview
+fun PreviewMainImage(){
+    MainImage(150.dp)
 }
