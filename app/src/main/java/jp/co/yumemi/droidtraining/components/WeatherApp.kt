ad4067336138864fd254f75cde80b7749c11a868
h_ideura <h_ideura@yumemi.co.jp>
2023-08-26 16:08:25 +0900

    fix: ActionButtonsのコールバック・テキスト追加

--- app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +22,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,7 +46,8 @@ fun WeatherApp(){
             WeatherInfo(weather)
             ActionButtons(modifier = Modifier
                 .padding(top = 80.dp)
-                .weight(1f))
+                .weight(1f)
+            )
         }
     }
 }
@@ -78,13 +81,20 @@ fun WeatherInfo(weather: WeatherInfo, modifier: Modifier = Modifier){
 }
 
 @Composable
-fun ActionButtons(modifier: Modifier = Modifier){
-    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-        Button( onClick = { /*TODO*/ }) {
-            Text(text = "Btn1")
+fun ActionButtons(
+    modifier: Modifier = Modifier,
+    onReloadClick: () -> Unit = {},
+    onNextClick: () -> Unit = {},
+){
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button( onClick = onReloadClick) {
+            Text(text = stringResource(id = R.string.reload))
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Btn2")
+        Button(onClick = onNextClick) {
+            Text(text = stringResource(id = R.string.next))
         }
     }
 }
