755d7e3c6ee51346e1ce15e3d6d27302ebce4192
h_ideura <h_ideura@yumemi.co.jp>
2023-08-28 16:58:58 +0900

    add: 天気フェッチ時の例外処理追加
    UnknownExceptionがきた場合ダイアログ表示

--- app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -32,13 +33,19 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.api.YumemiWeather
 import jp.co.yumemi.droidtraining.R
 import jp.co.yumemi.droidtraining.WeatherInfoData
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherApp(){
     val yumemiWeather = YumemiWeather(context = LocalContext.current)
+    var showErrorDialog: Boolean by remember {
+        mutableStateOf(false)
+    }
+
     var weatherInfoData by remember {
         mutableStateOf(
             WeatherInfoData(
@@ -49,6 +56,26 @@ fun WeatherApp(){
         )
     }
 
+    /** 天気を取得。例外があればエラーダイアログ表示 */
+    fun getWeather(){
+        try{
+            val weather = yumemiWeather.fetchThrowsWeather()
+            val newWeatherInfoData = weatherInfoData.copy(weather = weather)
+            weatherInfoData = newWeatherInfoData
+        }catch (e: UnknownException){
+            showErrorDialog = true
+        }
+    }
+    
+    WeatherFetchErrorDialog(
+        showDialog = showErrorDialog,
+        onDismissRequest = { showErrorDialog = false },
+        onReload = {
+            showErrorDialog = false
+            getWeather()
+        }
+    )
+
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -58,15 +85,12 @@ fun WeatherApp(){
         Column(modifier = Modifier.width(width)) {
             Spacer(modifier = Modifier.weight(1f))
             WeatherInfo(weatherInfoData)
+
             ActionButtons(modifier = Modifier
                 .padding(top = 80.dp)
                 .weight(1f),
                 onReloadClick = {
-                    weatherInfoData = WeatherInfoData(
-                        weather = yumemiWeather.fetchSimpleWeather(),
-                        lowestTemperature = 5,
-                        highestTemperature = 40
-                    )
+                    getWeather()
                 }
             )
         }
