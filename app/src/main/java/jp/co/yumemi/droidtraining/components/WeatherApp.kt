32dda29a78ac6bb8c973e4c075b923b2d7488b5f
h_ideura <h_ideura@yumemi.co.jp>
2023-08-28 18:35:14 +0900

    refactor: 無駄なアノテーション削除

--- app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/components/WeatherApp.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -37,7 +36,6 @@ import jp.co.yumemi.api.YumemiWeather
 import jp.co.yumemi.droidtraining.R
 import jp.co.yumemi.droidtraining.WeatherInfoData
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherApp(
     yumemiWeather:YumemiWeather = YumemiWeather(LocalContext.current),
