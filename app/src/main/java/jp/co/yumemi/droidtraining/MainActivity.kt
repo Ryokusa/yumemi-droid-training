3863ee7daa76c8f8ba917d596d6e50ed96b0db70
h_ideura <h_ideura@yumemi.co.jp>
2023-09-05 18:41:24 +0900

    add: Hiltが使えるように追加

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -3,9 +3,11 @@ package jp.co.yumemi.droidtraining
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.droidtraining.components.WeatherApp
 import jp.co.yumemi.droidtraining.theme.YumemiTheme
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
