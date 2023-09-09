ab34ef0370fdf60e64c4a8fd153385ab1b4832fb
h_ideura <h_ideura@yumemi.co.jp>
2023-09-09 16:31:35 +0900

    fix: activity破棄後も保持するよう修正
    saveStateHandle使用

--- app/src/main/java/jp/co/yumemi/droidtraining/WeatherMainViewModel.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/WeatherMainViewModel.kt
@@ -1,24 +1,44 @@
 package jp.co.yumemi.droidtraining
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.api.YumemiWeather
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 open class WeatherMainViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     private val yumemiWeather: YumemiWeather,
-    initialWeatherInfoData: WeatherInfoData
+    initialWeatherInfoData: WeatherInfoData,
 ): ViewModel() {
-
-    private val _isShowErrorDialog = MutableStateFlow(false)
+    private val isShowErrorDialogKey = "isShowErrorDialog"
+    private val _isShowErrorDialog = MutableStateFlow(
+        savedStateHandle.get<Boolean>(isShowErrorDialogKey) ?: false
+    )
     val isShowErrorDialog = _isShowErrorDialog.asStateFlow()
 
-    private val _weatherInfoData = MutableStateFlow(initialWeatherInfoData)
+    private val weatherInfoDataKey = "weatherInfoData"
+    private val _weatherInfoData = MutableStateFlow(
+        savedStateHandle.get<WeatherInfoData>(weatherInfoDataKey) ?: initialWeatherInfoData
+    )
     val weatherInfoData = _weatherInfoData.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            launch {
+                weatherInfoData.collect{ savedStateHandle[weatherInfoDataKey] = it }
+            }
+            launch {
+                isShowErrorDialog.collect { savedStateHandle[isShowErrorDialogKey] = it }
+            }
+        }
+    }
 
     fun reloadWeather(){
         try{
@@ -42,4 +62,8 @@ open class WeatherMainViewModel @Inject constructor(
 class FakeWeatherMainViewModel(
     yumemiWeather: YumemiWeather,
     initialWeatherInfoData: WeatherInfoData
-) : WeatherMainViewModel(initialWeatherInfoData = initialWeatherInfoData, yumemiWeather = yumemiWeather)
+) : WeatherMainViewModel(
+    initialWeatherInfoData = initialWeatherInfoData,
+    yumemiWeather = yumemiWeather,
+    savedStateHandle = SavedStateHandle()   //fake(empty)
+)
