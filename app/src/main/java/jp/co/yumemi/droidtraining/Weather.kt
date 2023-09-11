9887fcfc44988b0a9b48a753a2a708ee030c0341
h_ideura <h_ideura@yumemi.co.jp>
2023-09-11 16:47:20 +0900

    add: CurrentWeatherDataの変換関数追加

--- app/src/main/java/jp/co/yumemi/droidtraining/Weather.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/Weather.kt
@@ -1,5 +1,7 @@
 package jp.co.yumemi.droidtraining
 
+import jp.co.yumemi.api.UnknownException
+
 enum class WeatherType(val weather: String) {
     SUNNY("sunny"),
     CLOUDY("cloudy"),
@@ -12,6 +14,16 @@ enum class WeatherType(val weather: String) {
          *
          * @throws NoSuchElementException 要素がない場合
          */
-        fun of(weather: String): WeatherType = values().first{ it.weather == weather }
+        fun of(weather: String): WeatherType = values().first { it.weather == weather }
+
+        fun fromCurrentWeatherDataId(weatherId: Long): WeatherType {
+            if (weatherId.toInt() == 800) return SUNNY
+            return when (weatherId.toString().substring(0, 1).toInt()) {
+                2, 3, 5 -> RAINY
+                6 -> SNOW
+                7, 8 -> CLOUDY
+                else -> throw UnknownException()
+            }
+        }
     }
 }
--- app/src/main/java/jp/co/yumemi/droidtraining/model/WeatherInfoData.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/model/WeatherInfoData.kt
@@ -5,8 +5,10 @@ import androidx.annotation.DrawableRes
 import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.droidtraining.R
 import jp.co.yumemi.droidtraining.WeatherType
+import jp.co.yumemi.droidtraining.api.CurrentWeatherData
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
+import kotlin.math.roundToInt
 
 /** 仮天気情報
  * TODO:正式な形にする
@@ -45,6 +47,23 @@ data class WeatherInfoData(
                 // 該当するWeatherTypeがない場合
                 throw UnknownException()
             }
+        }
+
+        /**
+         * CurrentWeatherDataからの変換
+         * @throws UnknownException 不明な天気の場合
+         */
+        operator fun invoke(currentWeatherData: CurrentWeatherData): WeatherInfoData {
+            val weatherType = WeatherType.fromCurrentWeatherDataId(currentWeatherData.weather[0].id)
+            val highestTemperature = currentWeatherData.main.tempMax.roundToInt().toShort()
+            val lowestTemperature = currentWeatherData.main.tempMin.roundToInt().toShort()
+            val place = currentWeatherData.name
+            return WeatherInfoData(
+                weather = weatherType,
+                highestTemperature = highestTemperature,
+                lowestTemperature = lowestTemperature,
+                place = place
+            )
         }
     }
 
