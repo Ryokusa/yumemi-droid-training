3034178023eba0102adefd4317b8feebe25290fc
h_ideura <h_ideura@yumemi.co.jp>
2023-09-16 15:05:59 +0900

    refactor: コメント追加

--- app/src/main/java/jp/co/yumemi/droidtraining/Weather.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/Weather.kt
@@ -16,8 +16,14 @@ enum class WeatherType(val weather: String) {
          */
         fun of(weather: String): WeatherType = values().first { it.weather == weather }
 
+        /**
+         * CurrentWeatherDataの天気IDからWeatherType取得
+         * 詳細：https://openweathermap.org/weather-conditions
+         */
         fun fromCurrentWeatherDataId(weatherId: Long): WeatherType {
             if (weatherId.toInt() == 800) return SUNNY
+            
+            // ID頭文字で分岐
             return when (weatherId.toString().substring(0, 1).toInt()) {
                 2, 3, 5 -> RAINY
                 6 -> SNOW
