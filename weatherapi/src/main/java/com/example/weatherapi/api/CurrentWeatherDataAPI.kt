dd39e9f5bc7b2b2239c0430fa84702a26da20d0c
h_ideura <h_ideura@yumemi.co.jp>
2023-09-18 16:40:52 +0900

    fix: DIできるよう修正

--- weatherapi/src/main/java/com/example/weatherapi/api/CurrentWeatherDataAPI.kt
+++ weatherapi/src/main/java/com/example/weatherapi/api/CurrentWeatherDataAPI.kt
@@ -8,7 +8,23 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import java.io.IOException
 
-class CurrentWeatherDataAPI(private val apiKey: String) {
+private val contentType = "application/json".toMediaType()
+
+class CurrentWeatherDataAPI(
+    private val apiKey: String,
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .addInterceptor(JapaneseCurrentWeatherDataInterceptor())
+        .build(),
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(Json.asConverterFactory(contentType))
+        .build(),
+    private val currentWeatherDataService: CurrentWeatherDataService = retrofit.create(
+        CurrentWeatherDataService::class.java
+    )
+) {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
@@ -19,21 +35,6 @@ class CurrentWeatherDataAPI(private val apiKey: String) {
             NAGOYA(1856057),
         }
     }
-
-    private val contentType = "application/json".toMediaType()
-
-    private val client: OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        .addInterceptor(JapaneseCurrentWeatherDataInterceptor())
-        .build()
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(client)
-        .addConverterFactory(Json.asConverterFactory(contentType))
-        .build()
-
-    private val currentWeatherDataService = retrofit.create(CurrentWeatherDataService::class.java)
 
     suspend fun fetchCurrentWeatherData(cityId: CityId): CurrentWeatherData {
         try {
