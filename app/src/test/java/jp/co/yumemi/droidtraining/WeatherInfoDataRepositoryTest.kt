2e5d70844ac20c4c726240c9a14e8fadf9978b3d
h_ideura <h_ideura@yumemi.co.jp>
2023-09-19 15:26:34 +0900

    add: 天気リポジトリのテスト追加

---
+++ app/src/test/java/jp/co/yumemi/droidtraining/WeatherInfoDataRepositoryTest.kt
@@ -0,0 +1,114 @@
+package jp.co.yumemi.droidtraining
+
+import com.example.weatherapi.api.CurrentWeatherData
+import com.example.weatherapi.api.CurrentWeatherDataAPI
+import jp.co.yumemi.droidtraining.model.WeatherInfoData
+import jp.co.yumemi.droidtraining.repository.WeatherInfoDataRepositoryImpl
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class WeatherInfoDataRepositoryTest {
+    class FakeCurrentWeatherDataAPI(private val _fetchCurrentWeatherData: (cityId: CurrentWeatherDataAPI.CityId) -> CurrentWeatherData) :
+        CurrentWeatherDataAPI {
+        override suspend fun fetchCurrentWeatherData(cityId: CurrentWeatherDataAPI.CityId): CurrentWeatherData {
+            return _fetchCurrentWeatherData(cityId)
+        }
+    }
+
+    // 加工して使う用
+    val currentWeatherData = CurrentWeatherData(
+        coord = CurrentWeatherData.Coord(
+            lon = 139.6917,
+            lat = 35.6895
+        ),
+        weather = listOf(
+            CurrentWeatherData.Weather(
+                id = 803,
+                main = "Clouds",
+                description = "broken clouds",
+                icon = "04n"
+            )
+        ),
+        base = "stations",
+        main = CurrentWeatherData.Main(
+            temp = 289.5,
+            feelsLike = 289.52,
+            tempMin = 288.82,
+            tempMax = 290.15,
+            pressure = 1013,
+            humidity = 89
+        ),
+        visibility = 10000,
+        wind = CurrentWeatherData.Wind(
+            speed = 0.45,
+            deg = 0,
+            gust = 1.34
+        ),
+        clouds = CurrentWeatherData.Clouds(
+            all = 75
+        ),
+        dt = 1630548912,
+        sys = CurrentWeatherData.Sys(
+            type = 1,
+            id = 8074,
+            country = "JP",
+            sunrise = 1630499316,
+            sunset = 1630544869
+        ),
+        timezone = 32400,
+        id = 1850147,
+        name = "Tokyo",
+        cod = 200
+    )
+
+    @Test
+    fun isSameInitialWeatherInfoData() = runTest {
+        val initialWeatherInfoData = WeatherInfoData(
+            weather = WeatherType.SUNNY,
+            lowestTemperature = 5,
+            highestTemperature = 40,
+            place = "岐阜",
+        )
+        val repository = WeatherInfoDataRepositoryImpl(
+            initialWeatherInfoData = initialWeatherInfoData,
+            currentWeatherDataAPI = FakeCurrentWeatherDataAPI { currentWeatherData }
+        )
+        assert(repository.weatherInfoData.value == initialWeatherInfoData)
+    }
+
+    @Test
+    fun canUpdateWeatherInfoData() = runTest {
+        val initialWeatherInfoData = WeatherInfoData(
+            weather = WeatherType.RAINY,
+            lowestTemperature = 5,
+            highestTemperature = 40,
+            place = "岐阜",
+        )
+
+        // フェッチされる天気情報
+        val newWeatherInfoData = WeatherInfoData(
+            weather = WeatherType.SUNNY,
+            lowestTemperature = 20,
+            highestTemperature = 50,
+            place = "東京",
+        )
+        val newWeather = currentWeatherData.weather[0].copy(id = 800) //SUNNY id
+        val newMain = currentWeatherData.main.copy(
+            tempMin = newWeatherInfoData.lowestTemperature.toDouble(),
+            tempMax = newWeatherInfoData.highestTemperature.toDouble()
+        )
+        val newCurrentWeatherData = currentWeatherData.copy(
+            weather = listOf(newWeather),
+            main = newMain,
+            name = newWeatherInfoData.place
+        )
+
+        // テスト
+        val repository = WeatherInfoDataRepositoryImpl(
+            initialWeatherInfoData = initialWeatherInfoData,
+            currentWeatherDataAPI = FakeCurrentWeatherDataAPI { newCurrentWeatherData }
+        )
+        repository.updateWeatherInfoData()
+        assert(repository.weatherInfoData.value == newWeatherInfoData)
+    }
+}
