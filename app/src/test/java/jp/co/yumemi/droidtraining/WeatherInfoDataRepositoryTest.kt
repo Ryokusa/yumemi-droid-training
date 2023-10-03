d84ce7af6aa4fb8d438cf064645c01ffa60962e0
h_ideura <h_ideura@yumemi.co.jp>
2023-10-03 15:56:57 +0900

    add: Repository天気予報テスト

--- app/src/test/java/jp/co/yumemi/droidtraining/WeatherInfoDataRepositoryTest.kt
+++ app/src/test/java/jp/co/yumemi/droidtraining/WeatherInfoDataRepositoryTest.kt
@@ -6,6 +6,7 @@ import com.example.weatherapi.api.OpenWeatherDataAPI
 import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.droidtraining.model.WeatherInfoData
 import jp.co.yumemi.droidtraining.repository.WeatherInfoDataRepositoryImpl
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import java.time.LocalDateTime
@@ -155,5 +156,146 @@ class WeatherInfoDataRepositoryTest {
             ),
         )
         repository.updateWeatherInfoData()
+    }
+
+    private val fakeForecastDataList: ForecastDataList = ForecastDataList(
+        cod = "200",
+        message = 1,
+        cnt = 40,
+        list = listOf(
+            ForecastDataList.ForecastData(
+                dt = 1620034215,
+                main = CurrentWeatherData.Main(
+                    temp = 25.0,
+                    feelsLike = 2.0,
+                    tempMin = 10.0,
+                    tempMax = 29.0,
+                    pressure = 2,
+                    humidity = 4,
+                ),
+                weather = listOf(
+                    CurrentWeatherData.Weather(
+                        id = 500,
+                        main = "Rain",
+                        description = "light rain",
+                        icon = "10d",
+                    )
+                ),
+                clouds = CurrentWeatherData.Clouds(
+                    all = 100,
+                ),
+                wind = CurrentWeatherData.Wind(
+                    speed = 0.62,
+                    deg = 349,
+                    gust = 1.18,
+                ),
+                visibility = 10000,
+                pop = 0.32,
+                rain = CurrentWeatherData.Rain(
+                    the3H = 0.26,
+                ),
+                snow = CurrentWeatherData.Snow(
+                    the3H = 0.0,
+                ),
+                sys = ForecastDataList.Sys(
+                    pod = "d",
+                ),
+                dtText = "2022-08-30 15:00:00",
+            ),
+            ForecastDataList.ForecastData(
+                dt = 1620000000,
+                main = CurrentWeatherData.Main(
+                    temp = 0.0,
+                    feelsLike = 0.0,
+                    tempMin = 0.0,
+                    tempMax = 0.0,
+                    pressure = 0,
+                    humidity = 0,
+                ),
+                weather = listOf(
+                    CurrentWeatherData.Weather(
+                        id = 600,
+                        main = "",
+                        description = "",
+                        icon = "",
+                    )
+                ),
+                clouds = CurrentWeatherData.Clouds(
+                    all = 0,
+                ),
+                wind = CurrentWeatherData.Wind(
+                    speed = 0.0,
+                    deg = 0,
+                    gust = 0.0,
+                ),
+                visibility = 0,
+                pop = 0.0,
+                rain = CurrentWeatherData.Rain(
+                    the3H = 0.0,
+                ),
+                snow = CurrentWeatherData.Snow(
+                    the3H = 0.0,
+                ),
+                sys = ForecastDataList.Sys(
+                    pod = "d",
+                ),
+                dtText = "",
+            )
+        ),
+        city = ForecastDataList.City(
+            id = 3163858,
+            name = "3163858",
+            coord = CurrentWeatherData.Coord(
+                lon = 44.34,
+                lat = 10.99,
+            ),
+            country = "IT",
+            population = 4593,
+            timezone = 7200,
+            sunrise = 166183417,
+            sunset = 166182248,
+        ),
+    )
+
+    @Test
+    fun canUpdateForecastWeatherInfoData() = runTest {
+        val initialWeatherInfoData = WeatherInfoData(
+            weather = WeatherType.RAINY,
+            lowestTemperature = 5,
+            highestTemperature = 40,
+            place = "岐阜",
+            temperature = 10,
+            dateTime = LocalDateTime.now(),
+        )
+        val repository = WeatherInfoDataRepositoryImpl(
+            initialWeatherInfoData = initialWeatherInfoData,
+            openWeatherDataAPI = FakeOpenWeatherDataAPI(
+                _fetchCurrentWeatherData = { throw NotImplementedError() },
+                _fetch5day3hourForecastData = { fakeForecastDataList },
+            ),
+        )
+        repository.updateForecastWeatherInfoDataList()
+        val forecastWeatherDataList = WeatherInfoData(fakeForecastDataList)
+        assertEquals(repository.forecastWeatherInfoDataList.value, forecastWeatherDataList)
+    }
+
+    @Test(expected = UnknownException::class)
+    fun updateForecastWeatherData_isFailed() = runTest {
+        val initialWeatherInfoData = WeatherInfoData(
+            weather = WeatherType.RAINY,
+            lowestTemperature = 5,
+            highestTemperature = 40,
+            place = "岐阜",
+            temperature = 10,
+            dateTime = LocalDateTime.now(),
+        )
+        val repository = WeatherInfoDataRepositoryImpl(
+            initialWeatherInfoData = initialWeatherInfoData,
+            openWeatherDataAPI = FakeOpenWeatherDataAPI(
+                _fetchCurrentWeatherData = { throw NotImplementedError() },
+                _fetch5day3hourForecastData = { throw Exception() },
+            ),
+        )
+        repository.updateForecastWeatherInfoDataList()
     }
 }
