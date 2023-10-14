d64542777c137336339c6097476ae25db534f3be
h_ideura <h_ideura@yumemi.co.jp>
2023-10-14 18:32:05 +0900

    fix: テストできるようViewModel設定修正
    予報で別ViewModelを使用したためエラーが発生していた

--- app/src/main/java/jp/co/yumemi/droidtraining/viewmodels/FakeForecastWeatherViewModel.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/viewmodels/FakeForecastWeatherViewModel.kt
@@ -2,13 +2,14 @@ package jp.co.yumemi.droidtraining.viewmodels
 
 import jp.co.yumemi.droidtraining.model.WeatherInfoData
 import jp.co.yumemi.droidtraining.repository.FakeWeatherInfoDataRepository
+import jp.co.yumemi.droidtraining.repository.WeatherInfoDataRepository
 import jp.co.yumemi.droidtraining.usecases.GetForecastWeatherInfoDataListUseCase
 import jp.co.yumemi.droidtraining.usecases.UpdateForecastWeatherInfoDataListUseCase
 
 class FakeForecastWeatherViewModel(
     initialForecastWeatherInfoDataList: List<WeatherInfoData> = listOf(),
     updatedForecastWeatherInfoDataList: List<WeatherInfoData> = initialForecastWeatherInfoDataList,
-    fakeWeatherInfoDataRepository: FakeWeatherInfoDataRepository = FakeWeatherInfoDataRepository(
+    fakeWeatherInfoDataRepository: WeatherInfoDataRepository = FakeWeatherInfoDataRepository(
         initialForecastWeatherInfoDataList = initialForecastWeatherInfoDataList,
         updatedForecastWeatherInfoDataList = updatedForecastWeatherInfoDataList,
     ),
