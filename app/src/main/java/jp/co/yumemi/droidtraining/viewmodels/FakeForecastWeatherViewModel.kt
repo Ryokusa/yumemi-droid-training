3f2c5d502b80f16b2b78a984e44911de3be8f2b3
h_ideura <h_ideura@yumemi.co.jp>
2023-10-14 17:00:31 +0900

    add: FakeForecastWeatherViewModel追加

---
+++ app/src/main/java/jp/co/yumemi/droidtraining/viewmodels/FakeForecastWeatherViewModel.kt
@@ -0,0 +1,21 @@
+package jp.co.yumemi.droidtraining.viewmodels
+
+import jp.co.yumemi.droidtraining.model.WeatherInfoData
+import jp.co.yumemi.droidtraining.usecases.GetForecastWeatherInfoDataListUseCase
+import jp.co.yumemi.droidtraining.usecases.UpdateForecastWeatherInfoDataListUseCase
+
+class FakeForecastWeatherViewModel(
+    initialForecastWeatherInfoDataList: List<WeatherInfoData> = listOf(),
+    updatedForecastWeatherInfoDataList: List<WeatherInfoData> = initialForecastWeatherInfoDataList,
+    fakeWeatherInfoDataRepository: FakeWeatherInfoDataRepository = FakeWeatherInfoDataRepository(
+        initialForecastWeatherInfoDataList = initialForecastWeatherInfoDataList,
+        updatedForecastWeatherInfoDataList = updatedForecastWeatherInfoDataList,
+    ),
+) : ForecastWeatherViewModel(
+    getForecastWeatherInfoDataListUseCase = GetForecastWeatherInfoDataListUseCase(
+        fakeWeatherInfoDataRepository,
+    ),
+    updateForecastWeatherInfoDataListUseCase = UpdateForecastWeatherInfoDataListUseCase(
+        fakeWeatherInfoDataRepository,
+    ),
+)
