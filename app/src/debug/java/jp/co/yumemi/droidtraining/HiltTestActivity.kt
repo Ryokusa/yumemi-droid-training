3973b7dba47b3188bcf63bae38eb826a462953a3
h_ideura <h_ideura@yumemi.co.jp>
2023-10-30 16:20:22 +0900

    add:Hiltテスト用の偽アクティビティ追加

---
+++ app/src/debug/java/jp/co/yumemi/droidtraining/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package jp.co.yumemi.droidtraining
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
